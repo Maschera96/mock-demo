66865cbc5f8c3069f78c65de996c44f57e8a4bf6
Maschera <695498150@qq.com>
2024-03-09 01:06:55 +0800

    add: static data

--- mock/question.js
+++ mock/question.js
@@ -1,5 +1,6 @@
 const Mock = require('mockjs')
 const getQuestionList = require('./data/getQuestionList')
+const getComponentList = require('./data/getComponentList')
 
 const Random = Mock.Random
 
@@ -17,104 +18,8 @@ module.exports = [
                     desc: '问卷描述',
                     js: '',
                     css: '',
-                    componentList: [
-                        // Info
-                        {
-                            fe_id: Random.id(),
-                            type: 'questionInfo',
-                            title: '问卷信息',
-                            isHidden: false,
-                            isLocked: false,
-                            props: {
-                                title: '问卷标题',
-                                desc: '问卷描述'
-                            },
-                        },
-                        // Title
-                        {
-                            fe_id: Random.id(),
-                            type: 'questionTitle',
-                            title: '标题',
-                            isHidden: false,
-                            isLocked: false,
-                            props: {
-                                text: '个人信息调研',
-                                level: 1,
-                                 isCenter: false
-                            },
-                        },
-                        // Input
-                        {
-                            fe_id: Random.id(),
-                            type: 'questionInput',
-                            title: '输入框1',
-                            isHidden: false,
-                            isLocked: false,
-                            props: {
-                                title: '你的姓名',
-                                placeholder: '请输入姓名...'
-                            },
-                        },
-                        // Textarea
-                        {
-                            fe_id: Random.id(),
-                            type: 'questionTextarea',
-                            title: '多行输入',
-                            isHidden: false,
-                            isLocked: false,
-                            props: {
-                                title: '你的爱好',
-                                placeholder: '请输入...'
-                            },
-                        },
-                        // Paragraph
-                        {
-                            fe_id: Random.id(),
-                            type: 'questionParagraph',
-                            title: '段落',
-                            isHidden: false,
-                            isLocked: false,
-                            props: {
-                                text: '一行段落',
-                                isCenter: false
-                            },
-                        },
-                        // Radio
-                        {
-                            fe_id: Random.id(),
-                            type: 'questionRadio',
-                            title: '单选',
-                            isHidden: false,
-                            isLocked: false,
-                            props: {
-                                title: "单选标题",
-                                isVertical: false,
-                                options: [
-                                    { value: "item1", text: "选项1" },
-                                    { value: "item2", text: "选项2" },
-                                    { value: "item3", text: "选项3" },
-                                ],
-                                value: "",
-                            },
-                        },
-                        // Checkbox
-                        {
-                            fe_id: Random.id(),
-                            type: 'questionCheckbox',
-                            title: '多选',
-                            isHidden: false,
-                            isLocked: false,
-                            props: {
-                                title: "多选标题",
-                                isVertical: false,
-                                list: [
-                                    { value: "item1", text: "选项1", checked: false },
-                                    { value: "item2", text: "选项2", checked: false },
-                                    { value: "item3", text: "选项3", checked: false },
-                                ],
-                            },
-                        }
-                    ]
+                    isPublished: true,
+                    componentList: getComponentList()
                 }
             }
         }
